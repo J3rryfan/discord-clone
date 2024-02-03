5ec1ad256337b8dfe2598dccb3f4227ec2040ea3
Jerry Fan <Jerryfan71@gmail.com>
2024-02-03 12:06:57 -0800

    Add dark mode ModeToggle and Theme Provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/model-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className=" ">
       <div>This is a protected Route</div>
+      <ModeToggle />
     </div>
   );
 }
