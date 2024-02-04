6c04aa9285e1e1b2fd98df047f30d7781ae1210d
Jerry Fan <Jerryfan71@gmail.com>
2024-02-03 17:56:45 -0800

    Update environment variables and dependencies, add new pages and components

--- src/app/(auth)/(routes)/register/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Registerpage() {
-  return <div>Registerpage</div>;
-}
---
+++ src/lib/db.ts
@@ -0,0 +1,39 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirectToSignIn();
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
