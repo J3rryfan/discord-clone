6dfd8ca1b072e65e196fb5faaeeb7e53b037af8f
Jerry Fan <Jerryfan71@gmail.com>
2024-02-04 15:07:45 -0800

    Add NavigationItem component and ScrollArea component

--- src/components/navigation/navigation-side-bar.tsx
+++ src/components/navigation/navigation-side-bar.tsx
@@ -2,6 +2,9 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavigationAction from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import NavigationItem from "./navigation-item";
 
 export default async function NavigationSideBar() {
   const profile = await currentProfile();
@@ -10,7 +13,7 @@ export default async function NavigationSideBar() {
     return redirect("/");
   }
 
-  const server = await db.server.findMany({
+  const servers = await db.server.findMany({
     where: {
       members: {
         some: {
@@ -23,6 +26,18 @@ export default async function NavigationSideBar() {
   return (
     <div className=" flex flex-col items-center h-full w-full space-y-4 py-3 text-primary dark:bg-[#1E1F22]">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className=" flex-1 w-ful">
+        {servers.map((server) => (
+          <div key={server.id} className=" mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 }
