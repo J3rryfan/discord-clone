bab34b036922d8f0a28ae7cf39028038d541d7fd
Jerry Fan <Jerryfan71@gmail.com>
2024-02-06 12:18:56 -0800

    Add @radix-ui/react-select package and remove FileUpload component from CreateChannelModal

--- src/components/modals/create-channel-modal.tsx
+++ src/components/modals/create-channel-modal.tsx
@@ -4,12 +4,10 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import FileUpload from "../file-upload";
 
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -31,9 +29,6 @@ import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Server name is required" }),
-  image: z.string().min(1, {
-    message: "Server image is required",
-  }),
 });
 
 export default function CreateChannelModal() {
@@ -48,7 +43,6 @@ export default function CreateChannelModal() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      image: "",
     },
   });
 
@@ -62,19 +56,6 @@ export default function CreateChannelModal() {
 
     try {
       await axios.post("/api/servers", values); // axios way
-
-      // const response = await fetch("/api/servers", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(values),
-      // });
-
-      // if (!response.ok) {
-      //   throw new Error("Failed to create server");
-      // }
-
       form.reset();
       router.refresh();
       onClose();
@@ -94,46 +75,25 @@ export default function CreateChannelModal() {
         <DialogContent className=" bg-white text-black p-0 overflow-hidden">
           <DialogHeader className=" pt-8 px-6">
             <DialogTitle className=" text-2xl text-center font-bold">
-              Customize your server
+              Create Channel
             </DialogTitle>
-            <DialogDescription className=" text-center text-zinc-500">
-              Give your server a personality with a name and image. You can
-              always change it later
-            </DialogDescription>
           </DialogHeader>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
               <div className=" space-y-8 px-6">
-                <div className=" flex items-center justify-center text-center">
-                  <FormField
-                    control={form.control}
-                    name="image"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <FileUpload
-                            endpoint="serverImage"
-                            value={field.value}
-                            onChange={field.onChange}
-                          />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
-                </div>
                 <FormField
                   control={form.control}
                   name="name"
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel className="uppercase text-sm font-bold text-zinc-500 dark: text-secondary/70">
-                        Server name
+                        Channel name
                       </FormLabel>
                       <FormControl>
                         <Input
                           disabled={isLoading}
                           className=" bg-zinc-300/50  border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                          placeholder="Enter server name"
+                          placeholder="Enter channel name"
                           {...field}
                         />
                       </FormControl>
