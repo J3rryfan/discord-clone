a112effbe0f87f148cbd1484fb4335fac9c17519
Jerry Fan <Jerryfan71@gmail.com>
2024-02-06 12:15:09 -0800

    Add CreateChannelModal component and update ServerHeader to handle create channel action

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import CreateServerModal from "@/components/modals/create-server-modal";
 import InviteServerModal from "../modals/invite-server-modal";
 import EditServerModal from "../modals/edit-server-modal";
 import MembersModal from "../modals/members-modal";
+import CreateChannelModal from "../modals/create-channel-modal";
 
 export default function ModalProvider() {
   const [isMounted, setIsMounted] = useState(false);
@@ -23,6 +24,7 @@ export default function ModalProvider() {
       <InviteServerModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 }
