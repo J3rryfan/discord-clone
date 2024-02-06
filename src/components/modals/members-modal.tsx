d7fcded45c27a91e23fe470a2ec30d3994445998
Jerry Fan <Jerryfan71@gmail.com>
2024-02-05 21:35:49 -0800

    Add @radix-ui/react-avatar package and update MembersModal component

--- src/components/modals/members-modal.tsx
+++ src/components/modals/members-modal.tsx
@@ -13,47 +13,15 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Check, Copy, RefreshCcw } from "lucide-react";
-import { useOrigin } from "@/hooks/use-origin";
-import { useState } from "react";
-import axios from "axios";
+import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "../ui/scroll-area";
+import UserAvatar from "../user-avatar";
 
 export default function MembersModal() {
   const { isOpen, onClose, type, data, onOpen } = useModal();
-  const origin = useOrigin();
 
   const isModalOpen = isOpen && type === "members";
-  const { server } = data;
-
-  const [copied, setCopied] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-
-  const onCopy = () => {
-    navigator.clipboard.writeText(inviteUrl);
-    setCopied(true);
-
-    setTimeout(() => {
-      setCopied(false);
-    }, 1000);
-  };
-
-  const onNew = async () => {
-    try {
-      setIsLoading(true);
-
-      const response = await axios.patch(
-        `/api/servers/${server?.id}/invite-code`
-      );
-
-      onOpen("invite", { server: response.data });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const { server } = data as { server: ServerWithMembersWithProfiles };
 
   return (
     <div>
@@ -61,44 +29,19 @@ export default function MembersModal() {
         <DialogContent className=" bg-white text-black p-0 overflow-hidden">
           <DialogHeader className=" pt-8 px-6">
             <DialogTitle className=" text-2xl text-center font-bold">
-              Invite your Friends
+              Manage Members
             </DialogTitle>
             <DialogDescription className=" text-center text-zinc-500">
-              Give your server a personality with a name and image. You can
-              always change it later
+              {server?.members?.length} Members
             </DialogDescription>
           </DialogHeader>
-          <div className="p-6">
-            <Label className=" uppercase text-sm font-bold text-zinc-500 dark:text-secondary/70">
-              Server Invite Link
-            </Label>
-
-            <div className=" flex items-center mt-2 gap-x-2">
-              <Input
-                disabled={isLoading}
-                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                value={inviteUrl}
-              />
-              <Button size="icon" onClick={onCopy} disabled={isLoading}>
-                {copied ? (
-                  <Check className="w-4 h-4" />
-                ) : (
-                  <Copy className="w-4 h-4" />
-                )}
-              </Button>
-            </div>
-
-            <Button
-              disabled={isLoading}
-              variant="link"
-              size="sm"
-              className=" text-sm text-zinc-500 mt-4"
-              onClick={onNew}
-            >
-              Generate new invite link
-              <RefreshCcw className="w-4 h-4 ml-2" />
-            </Button>
-          </div>
+          <ScrollArea className="mt-8 max-h-[420px] pr-6">
+            {server?.members?.map((member) => (
+              <div className=" flex items-center gap-x-2 mb-6" key={member.id}>
+                <UserAvatar src={member.profile.imageUrl} />
+              </div>
+            ))}
+          </ScrollArea>
         </DialogContent>
       </Dialog>
     </div>
