d400280a03c8a3129477d8d44ea681be45c85770
Jerry Fan <Jerryfan71@gmail.com>
2024-02-05 20:56:39 -0800

    Add EditServerModal and enable server settings in ServerHeader

---
+++ src/components/modals/edit-server-modal.tsx
@@ -0,0 +1,156 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import FileUpload from "../file-upload";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Server name is required" }),
+  image: z.string().min(1, {
+    message: "Server image is required",
+  }),
+});
+
+export default function EditServerModal() {
+  const { isOpen, onClose, type } = useModal();
+
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "editServer";
+
+  // Define youre form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      image: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+
+    try {
+      await axios.post("/api/servers", values); // axios way
+
+      // const response = await fetch("/api/servers", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(values),
+      // });
+
+      // if (!response.ok) {
+      //   throw new Error("Failed to create server");
+      // }
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <div>
+      <Dialog open={isModalOpen} onOpenChange={handleClose}>
+        <DialogContent className=" bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className=" pt-8 px-6">
+            <DialogTitle className=" text-2xl text-center font-bold">
+              Customize your server
+            </DialogTitle>
+            <DialogDescription className=" text-center text-zinc-500">
+              Give your server a personality with a name and image. You can
+              always change it later
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
+              <div className=" space-y-8 px-6">
+                <div className=" flex items-center justify-center text-center">
+                  <FormField
+                    control={form.control}
+                    name="image"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <FileUpload
+                            endpoint="serverImage"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-sm font-bold text-zinc-500 dark: text-secondary/70">
+                        Server name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className=" bg-zinc-300/50  border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                          placeholder="Enter server name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="bg-grey-100 px-6 py-4">
+                <Button disabled={isLoading} variant="primary">
+                  Create
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
