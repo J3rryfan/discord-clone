7fde8d4bb39b1ac9570387ad5014d9e81cffbe11
Jerry Fan <Jerryfan71@gmail.com>
2024-02-06 16:47:17 -0800

    Update server sidebar and server search components

--- src/components/server/sever-search.tsx
+++ src/components/server/sever-search.tsx
@@ -1,4 +1,18 @@
+"use client";
 import { SearchIcon } from "lucide-react";
+import { useState } from "react";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
 
 interface ServerSearchProps {
   data: {
@@ -14,9 +28,14 @@ interface ServerSearchProps {
   }[];
 }
 export default function ServerSearch({ data }: ServerSearchProps) {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <button className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      >
         <SearchIcon className="w-4 h-4 textzinc-500 dark:text-zinc-400" />
         <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
           Search
@@ -25,6 +44,27 @@ export default function ServerSearch({ data }: ServerSearchProps) {
           <span className="text-xs">⌘</span>K
         </kbd>
       </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all Channels and Members" />
+        <CommandList>
+          <CommandEmpty>No Results found</CommandEmpty>
+          {data.map(({ label, type, data }) => {
+            if (!data?.length) return null;
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data?.map(({ id, icon, name }) => {
+                  return (
+                    <CommandItem key={id}>
+                      {icon}
+                      <span>{name}</span>
+                    </CommandItem>
+                  );
+                })}
+              </CommandGroup>
+            );
+          })}
+        </CommandList>
+      </CommandDialog>
     </>
   );
 }
