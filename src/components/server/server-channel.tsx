7e896fb9e489d41564df2752ba256b1a45a5b563
Jerry Fan <Jerryfan71@gmail.com>
2024-02-07 09:29:51 -0800

    Add DeleteChannelModal and useModal hook

--- src/components/server/server-channel.tsx
+++ src/components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import ActionTooltip from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -23,6 +24,7 @@ export default function ServerChannel({
   server,
   role,
 }: ServerChannelProps) {
+  const { onOpen } = useModal();
   const router = useRouter();
   const params = useParams();
 
@@ -52,7 +54,7 @@ export default function ServerChannel({
             <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
           <ActionTooltip label="Delete">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" onClick={() => onOpen("deleteChannel")} />
           </ActionTooltip>
         </div>
       )}
