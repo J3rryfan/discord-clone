a2bc99c00d954efd6ed9b6f51c427469773d0c39
Jerry Fan <Jerryfan71@gmail.com>
2024-02-06 18:35:59 -0800

    Update server section and sidebar components

--- src/components/server/server-sidebar.tsx
+++ src/components/server/server-sidebar.tsx
@@ -10,6 +10,7 @@ import { Separator } from "../ui/separator";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
 import ServerSection from "./server-section";
 import ServerChannel from "./server-channel";
+import ServerMember from "./server-member";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -137,14 +138,16 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
                 role={role}
                 label="Text Channels"
               />
-              {textChannels.map((channel) => (
-                <ServerChannel
-                  key={channel.id}
-                  channel={channel}
-                  role={role}
-                  server={server}
-                />
-              ))}
+              <div className="space-y-[2px]">
+                {textChannels.map((channel) => (
+                  <ServerChannel
+                    key={channel.id}
+                    channel={channel}
+                    role={role}
+                    server={server}
+                  />
+                ))}
+              </div>
             </div>
           )}
           {!!audioChannels?.length && (
@@ -155,14 +158,16 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
                 role={role}
                 label="Voice Channels"
               />
-              {audioChannels.map((channel) => (
-                <ServerChannel
-                  key={channel.id}
-                  channel={channel}
-                  role={role}
-                  server={server}
-                />
-              ))}
+              <div className="space-y-[2px]">
+                {audioChannels.map((channel) => (
+                  <ServerChannel
+                    key={channel.id}
+                    channel={channel}
+                    role={role}
+                    server={server}
+                  />
+                ))}
+              </div>
             </div>
           )}
           {!!videoChannels?.length && (
@@ -173,14 +178,32 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
                 role={role}
                 label="Voice Channels"
               />
-              {videoChannels.map((channel) => (
-                <ServerChannel
-                  key={channel.id}
-                  channel={channel}
-                  role={role}
-                  server={server}
-                />
-              ))}
+              <div className="space-y-[2px]">
+                {videoChannels.map((channel) => (
+                  <ServerChannel
+                    key={channel.id}
+                    channel={channel}
+                    role={role}
+                    server={server}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+
+          {!!members?.length && (
+            <div className="mb-2">
+              <ServerSection
+                sectionType="members"
+                role={role}
+                label="Members"
+                server={server}
+              />
+              <div className="space-y-[2px]">
+                {members.map((member) => (
+                  <ServerMember />
+                ))}
+              </div>
             </div>
           )}
         </Separator>
