52621ec27c1a6d83c0ee600eaa2e6aa665541c39
Jerry Fan <Jerryfan71@gmail.com>
2024-02-06 18:10:29 -0800

    Update lucide-react imports and add action tooltips

--- src/components/server/server-channel.tsx
+++ src/components/server/server-channel.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { Hash, Mic, Video } from "lucide-react";
+import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import ActionTooltip from "@/components/action-tooltip";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -32,7 +32,8 @@ export default function ServerChannel({
     <button
       onClick={() => {}}
       className={cn(
-        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1"
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.id === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
       )}
     >
       <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
@@ -45,6 +46,19 @@ export default function ServerChannel({
       >
         {channel.name}
       </p>
+      {channel.name !== "general" && role !== MemberRole.GUEST && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <ActionTooltip label="Edit">
+            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+          <ActionTooltip label="Delete">
+            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+        </div>
+      )}
+      {channel.name === "general" && (
+        <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400 " />
+      )}
     </button>
   );
 }
