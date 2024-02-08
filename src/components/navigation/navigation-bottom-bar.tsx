c704c882a881b6b2b1f78888663d49bb61e1e0e0
Jerry Fan <Jerryfan71@gmail.com>
2024-02-07 18:39:34 -0800

    Refactor navigation bottom bar component

--- src/components/navigation/navigation-bottom-bar.tsx
+++ src/components/navigation/navigation-bottom-bar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
-import NavigationAction from "./navigation-action";
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
+import { Bell, Home, MessageCircleMore } from "lucide-react";
+import NavigationBottomAction from "./navigation-action-bottom";
 
 export default async function NavigationBottomBar() {
   const profile = await currentProfile();
@@ -12,13 +13,31 @@ export default async function NavigationBottomBar() {
   }
 
   return (
-    <div className=" flex items-center justify-center space-x-4 dark:bg-[#1E1F22]">
-      <Link href={"/"}>Servers</Link>
-      <Link href={"/"}>Message</Link>
-      <Link href={"/"}>
-        <NavigationAction />
+    <div className=" flex items-center justify-center space-x-4 dark:bg-[#1E1F22] ">
+      <Link
+        href={"/"}
+        className="flex flex-col items-center text-sm hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition p-2 rounded-md "
+      >
+        <Home className="h-4 w-4 mb-1" />
+        Servers
       </Link>
-      <Link href={"/"}>Notification</Link>
+      <Link
+        href={"/"}
+        className="flex flex-col items-center text-sm hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition p-2 rounded-md"
+      >
+        <MessageCircleMore className="h-4 w-4 mb-1" />
+        Message
+      </Link>
+      <Link href={"/"}>
+        <NavigationBottomAction />
+      </Link>
+      <Link
+        href={"/"}
+        className="flex flex-col items-center text-sm hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition p-2 rounded-md"
+      >
+        <Bell className="h-4 w-4 mb-1" />
+        Notification
+      </Link>
       <Link href={"/"}>
         <UserButton
           afterSignOutUrl="/"
