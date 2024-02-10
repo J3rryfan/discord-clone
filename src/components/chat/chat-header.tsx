fe232493bc835340706f6b9fd68207ac57b5916e
Jerry Fan <Jerryfan71@gmail.com>
2024-02-10 15:31:31 -0800

    Update chat header component and install sheet from shadcn

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,4 +1,4 @@
-import { Hash } from "lucide-react";
+import { Hash, Menu } from "lucide-react";
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -14,7 +14,11 @@ export default function ChatHeader({
 }: ChatHeaderProps) {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      ChatHeader
+      <Menu />
+      {type === "channel" && (
+        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
   );
 }
