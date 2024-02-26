588bef72a3149d565e9c3c1b991b0ca17048d69e
Jerry Fan <Jerryfan71@gmail.com>
2024-02-25 22:28:45 -0800

    Add Lucide-React icons and useState hook

--- src/components/chat/chat-item.tsx
+++ src/components/chat/chat-item.tsx
@@ -3,8 +3,10 @@
 import { Member, MemberRole, Profile } from "@prisma/client";
 import UserAvatar from "@/components/user-avatar";
 import ActionTooltip from "@/components/action-tooltip";
-import { FileIcon, ShieldCheck } from "lucide-react";
+import { Edit, FileIcon, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface ChatItemProps {
   id: string,
@@ -42,6 +44,8 @@ export default function ChatItem({
   socketUrl,
   socketQuery,
 } : ChatItemProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const fileType = fileUrl?.split('.').pop();
 
@@ -96,8 +100,34 @@ export default function ChatItem({
             </div>
           )}
 
+          {!fileUrl && !isEditing && (
+            <p className={cn (
+              "text-sm text-zinc-600 dark:text-zinc-300",
+              deleted && "italic  text-zinc-500 dark:text-zinc-400 text-xs mt-1"
+            )}>
+              {content}
+              {
+                isUpdated && !deleted && (
+                  <span className=" text-[10px] mx-2 text-zinc-500 dark:text-zinc-400">
+                    (edited)
+                  </span>
+                )}
+            </p>
+          )}
         </div>
       </div>
+      {canDeleteMessage && (
+        <div className=" hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+          {canEditMessage && (
+            <ActionTooltip label="Edit">
+              <Edit className=" cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"/>
+            </ActionTooltip>
+          )}
+           <ActionTooltip label="Delete">
+              <Trash className=" cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"/>
+            </ActionTooltip>
+        </div>
+      )}
     </div>
   )
 }
