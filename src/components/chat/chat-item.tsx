9134c994bce2a541ad648d65f0ed6dcc2dbf7282
Jerry Fan <Jerryfan71@gmail.com>
2024-02-26 10:55:04 -0800

    Add form for editing chat messages and implement escape key to cancel the input when editing

--- src/components/chat/chat-item.tsx
+++ src/components/chat/chat-item.tsx
@@ -1,12 +1,23 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import axios from "axios";
+import qs from "query-string"
 
+
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { Edit, FileIcon, ShieldCheck, Trash } from "lucide-react";
 import { Member, MemberRole, Profile } from "@prisma/client";
+
 import UserAvatar from "@/components/user-avatar";
 import ActionTooltip from "@/components/action-tooltip";
-import { Edit, FileIcon, ShieldCheck, Trash } from "lucide-react";
-import Image from "next/image";
-import { useState } from "react";
-import { cn } from "@/lib/utils";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface ChatItemProps {
   id: string,
@@ -32,6 +43,11 @@ const roleIconMap = {
 }
 
 
+const formSchema = z.object({
+  content: z.string().min(1),
+})
+
+
 export default function ChatItem({
   id,
   content,
@@ -46,6 +62,38 @@ export default function ChatItem({
 } : ChatItemProps) {
   const [isEditing, setIsEditing] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      if (event.key === "Escape" || event.keyCode === 27) {
+        setIsEditing(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+
+  }, [])
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: content
+    }
+  });
+
+  const onSubmit = (values) => {
+    console.log(values)
+  };
+
+  useEffect(() => {
+    form.reset({
+      content: content,
+    })
+  },[content])
 
   const fileType = fileUrl?.split('.').pop();
 
@@ -114,13 +162,49 @@ export default function ChatItem({
                 )}
             </p>
           )}
+          {!fileUrl && isEditing && (
+            <Form {...form}>
+              <form 
+              className=" flex items-center w-full gap-x-2 pt-2"
+              onSubmit={form.handleSubmit(onSubmit)}>
+               <FormField
+                  control={form.control}
+                  name="content"
+                  render={({ field }) => (
+                    <FormItem className=" flex-1">
+                      <FormControl>
+                        <div className=" relative w-full">
+                          <Input 
+                          className=" p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                          placeholder="Edited message"
+                          {...field}
+                           
+                          />
+                        </div>
+                      </FormControl>
+                      
+                    </FormItem>
+                   )}
+                  />
+
+                  <Button size="sm" variant="primary">
+                    Save
+                  </Button>
+              </form>
+              <span className="text-[10px] mt-1 text-zinc-400">
+                Press escape to cancel, enter to save
+              </span>
+            </Form>
+          )}
         </div>
       </div>
       {canDeleteMessage && (
         <div className=" hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
           {canEditMessage && (
             <ActionTooltip label="Edit">
-              <Edit className=" cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"/>
+              <Edit
+               onClick={() => setIsEditing(true)}
+               className=" cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"/>
             </ActionTooltip>
           )}
            <ActionTooltip label="Delete">
