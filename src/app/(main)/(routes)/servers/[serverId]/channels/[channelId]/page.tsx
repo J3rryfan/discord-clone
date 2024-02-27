4edcdfb6aa0fe158ba67d2ab24a430b432c45af9
Jerry Fan <Jerryfan71@gmail.com>
2024-02-26 17:43:15 -0800

    Add ChannelType and MediaRoom components

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation";
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
 import ChatMessages from "@/components/chat/chat-messages";
+import { ChannelType } from "@prisma/client";
+import { MediaRoom } from "@/components/media-room";
 
 interface ChannelIdPageProps {
   params: {
@@ -44,7 +46,9 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
         serverId={channel.serverId}
         type="channel"
       />
-     <ChatMessages
+      {channel.type === ChannelType.TEXT && (
+        <>
+        <ChatMessages
             member={member}
             name={channel.name}
             chatId={channel.id}
@@ -58,12 +62,31 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
             paramKey="channelId"
             paramValue={channel.id}
           />
-      <ChatInput
-        name={channel.name}
-        type="channel"
-        apiUrl="/api/socket/messages"
-        query={{ channelId: channel.id, serverId: channel.serverId }}
-      />
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{ channelId: channel.id, serverId: channel.serverId }}
+          />
+        </>
+      )}
+
+      {channel.type === ChannelType.AUDIO && (
+        <MediaRoom 
+          chatId={channel.id}
+          video={false}
+          audio={true}
+        />
+      )}
+
+      {channel.type === ChannelType.VIDEO && (
+          <MediaRoom 
+            chatId={channel.id}
+            video={true}
+            audio={true}
+          />
+        )}
+     
     </div>
   );
 }
