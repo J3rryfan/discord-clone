059de3207178634544510a0006041e8dab8a9b38
Jerry Fan <Jerryfan71@gmail.com>
2024-02-26 18:42:44 -0800

    Refactor ServerSection component

--- src/components/server/server-section.tsx
+++ src/components/server/server-section.tsx
@@ -7,13 +7,13 @@ import { Button } from "../ui/button";
 import { Plus, Settings } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 
-interface SeverSectionProps {
+interface ServerSectionProps {
   label: string;
   role?: MemberRole;
   sectionType: "channels" | "members";
   channelType?: ChannelType;
   server?: ServerWithMembersWithProfiles;
-}
+};
 
 export default function ServerSection({
   label,
@@ -21,35 +21,34 @@ export default function ServerSection({
   sectionType,
   channelType,
   server,
-}: SeverSectionProps) {
+}: ServerSectionProps) {
   const { onOpen } = useModal();
 
   return (
-    <div className=" flex items-center justify-between py-2">
-      <p className=" text-sm uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+    <div className="flex items-center justify-between py-2">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
         {label}
       </p>
       {role !== MemberRole.GUEST && sectionType === "channels" && (
         <ActionTooltip label="Create Channel" side="top">
-          <Button
-            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          <button
             onClick={() => onOpen("createChannel", { channelType })}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <Plus className="h-4 w-4" />
-          </Button>
+          </button>
         </ActionTooltip>
       )}
-
       {role === MemberRole.ADMIN && sectionType === "members" && (
         <ActionTooltip label="Manage Members" side="top">
-          <Button
+          <button
+            onClick={() => onOpen("members", { server })}
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-            onClick={() => onOpen("members", { server: server })}
           >
             <Settings className="h-4 w-4" />
-          </Button>
+          </button>
         </ActionTooltip>
       )}
     </div>
-  );
+  )
 }
