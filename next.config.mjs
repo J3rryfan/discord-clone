8a776d895efab344913b2e9ef971c20e44bed635
Jerry Fan <Jerryfan71@gmail.com>
2024-02-26 18:25:39 -0800

    Add webpack configuration for external modules and update remotePatterns in next.config.mjs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "uploadthing.com",
+        protocol: 'https',
+        hostname: 'uploadthing.com',
       },
 
       {
-        protocol: "https",
-        hostname: "utfs.io",
+        protocol: 'https',
+        hostname: 'utfs.io',
       },
     ],
   },
