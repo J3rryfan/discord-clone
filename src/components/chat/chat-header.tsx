6329520a126c17bcc8836cbf67b6c21c1e80b4e8
Jerry Fan <Jerryfan71@gmail.com>
2024-02-26 17:58:21 -0800

    Add video functionality to MemberIdPage and ChatHeader

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 import MobileToggle from "../mobile-toggle";
 import UserAvatar from "../user-avatar";
 import SocketIndicator from "@/components/socket-indicator";
+import { ChatVideoButton } from "./chat-video-button";
 interface ChatHeaderProps {
   serverId: string;
   name: string;
@@ -26,6 +27,9 @@ export default function ChatHeader({
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center ">
+        {type === "conversation" && (
+          <ChatVideoButton />
+        )}
         <SocketIndicator />
       </div>
     </div>
